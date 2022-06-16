96f36d0d800f188ccd59db2919da052040570724
degomos <degomosigor@hotmail.com>
2022-06-16 18:23:04 +0200

    Safe check frame

--- Sources/PagerTabItemModifier.swift
+++ Sources/PagerTabItemModifier.swift
@@ -8,24 +8,26 @@
 import SwiftUI
 
 struct PagerTabItemModifier<NavTabView: View>: ViewModifier {
-
+    
     private var navTabView: () -> NavTabView
-
+    
     init(navTabView: @escaping () -> NavTabView) {
         self.navTabView = navTabView
     }
-
+    
     func body(content: Content) -> some View {
         GeometryReader { reader in
             content
                 .onAppear {
                     DispatchQueue.main.async {
                         let frame = reader.frame(in: .named("PagerViewScrollView"))
-                        index = Int(round(frame.minX / frame.width))
                         let tabView = navTabView()
                         let tabViewDelegate = tabView as? PagerTabViewDelegate
-                        dataStore.setView(AnyView(tabView), at: index)
-                        dataStore.setTabViewDelegate(tabViewDelegate, at: index)
+                        if !frame.isNull, !frame.isEmpty {
+                            index = Int(round(frame.minX / frame.width))
+                            dataStore.setView(AnyView(tabView), at: index)
+                            dataStore.setTabViewDelegate(tabViewDelegate, at: index)
+                        }
                     }
                 }.onDisappear {
                     dataStore.items[index]?.tabViewDelegate?.setState(state: .normal)
@@ -33,7 +35,7 @@ struct PagerTabItemModifier<NavTabView: View>: ViewModifier {
                 }
         }
     }
-
+    
     @EnvironmentObject private var dataStore: DataStore
     @EnvironmentObject private var settings: PagerSettings
     @State private var index = -1
